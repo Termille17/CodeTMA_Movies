1c9696da501c42e6c2b627adadc605fc1b78384a
Termille17 <95466065+Termille17@users.noreply.github.com>
2025-03-06 12:11:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
+import MovieCard from "./MovieCard";
+import MovieDetails from "./MovieDetails"; // Import the new page
+import SearchIcon from "./search.svg";
+import "./App.css";
+
+const API_URL = "http://www.omdbapi.com?apikey=cf20b56c";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("Avengers");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search || []);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <h1>CodeTMA Movies.</h1>
+        
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <div className="search">
+                  <input
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    placeholder="Search for movies"
+                  />
+                  <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                  />
+                </div>
+
+                {movies.length > 0 ? (
+                  <div className="container">
+                    {movies.map((movie) => (
+                      <MovieCard key={movie.imdbID} movie={movie} />
+                    ))}
+                  </div>
+                ) : (
+                  <div className="empty">
+                    <h2>No movies found</h2>
+                  </div>
+                )}
+              </>
+            }
+          />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
